786de3e6ab9b729c35c003a8af619eb8c089c69d
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-01-16 22:26:57 +0300

    [Add] - JavaFX methods and overrides

--- Calc/src/calc/Run.java
+++ Calc/src/calc/Run.java
@@ -1,13 +1,30 @@
 package calc;
+//necessary components for javafx
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
-
-public class Run
+public class Run extends Application
 {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        
+        
+    }
  
     public static void main(String[] args)
     {
         
-        
+        launch(args);
         
     }
     
